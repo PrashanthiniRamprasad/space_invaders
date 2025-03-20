ca2656061bfa7d86d8e9c3d0e33b0be6d408c5b8
PrashanthiniRamprasad <prashanthini.ramprasad@gmail.com>
2025-03-20 17:57:07 +0100

    Update space_invaders.py

--- space_invaders.py
+++ space_invaders.py
@@ -28,3 +28,55 @@ BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
 # intialize health
 red_health=10
 yellow_health=10
+
+class Spaceschip(pygame.sprite.Sprite):
+    def __init__(self,image,angle, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.image = pygame.transform.rotate(pygame.transform.scale(image, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), angle)
+        self.rect = self.image.get_rect()
+    def move_horizontal(self, VEL):
+        self.x += VEL
+        if player==1:
+            if self.rect.left<=0 or self.rect.right>=BORDER.left:
+                self.rect.move_ip(-VEL,0)
+        if player==2:
+            if self.rect.left<=BORDER.right or self.rect.right>=WIDTH:
+                self.rect.move_ip(-VEL,0)
+    def move_vertical(self, VEL):
+        self.rect.move_ip(0,VEL)
+        if self.rect.top<=0 or self.rect.bottom>=HEIGHT:
+            self.rect.move_ip(0,-VEL)
+red = Spaceschip(RED_SPACESHIP_IMAGE, 0, 100, 200)
+yellow = Spaceschip(YELLOW_SPACESHIP_IMAGE, 180, 700, 200) 
+sprites = pygame.sprite.Group()
+sprites.add(red)
+sprites.add(yellow)
+def draw_window():
+        WIN.blit(SPACE, (0, 0))
+        pygame.draw.rect(WIN, "black", BORDER)
+        red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, "white")
+        yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, "white")
+        WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+        WIN.blit(yellow_health_text, (10, 10))
+def draw_bullets():
+        for bullet in red_bullets:
+            pygame.draw.rect(WIN, "red", bullet)
+            bullet.x -= BULLET_VEL
+            
+        for bullet in yellow_bullets:
+            pygame.draw.rect(WIN, "yellow", bullet)
+            bullet.x += BULLET_VEL
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+
+red_bullets = []
+yellow_bullets = []
+
+def handle_bullets():
+     
+
+
+    
+
